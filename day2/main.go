efe8f6411d4a04bd7631a073dec1159200992eac
Ivaylo Bratoev <ivo@st6.io>
2022-12-02 17:04:44 +0200

    Day 2 part 1 solution

---
+++ day2/main.go
@@ -0,0 +1,58 @@
+package day2
+
+import (
+	_ "embed"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+type Shape int
+
+const (
+	Rock     Shape = 1
+	Paper          = 2
+	Scissors       = 3
+)
+
+var stringToShape = map[byte]Shape{
+	'X': Rock,
+	'Y': Paper,
+	'Z': Scissors,
+	'A': Rock,
+	'B': Paper,
+	'C': Scissors,
+}
+
+func ScoreRound(opponentShape Shape, myShape Shape) int {
+	if opponentShape == myShape {
+		return 3
+	}
+
+	if myShape == Rock && opponentShape == Scissors {
+		return 6
+	}
+
+	if myShape == Paper && opponentShape == Rock {
+		return 6
+	}
+
+	if myShape == Scissors && opponentShape == Paper {
+		return 6
+	}
+
+	return 0
+}
+
+func Day2() int {
+	var total = 0
+
+	for _, line := range strings.Split(strings.TrimRight(input, "\n"), "\n") {
+		var opponentShape = stringToShape[line[0]]
+		var myShape = stringToShape[line[2]]
+		total += ScoreRound(opponentShape, myShape) + int(myShape)
+	}
+
+	return total
+}
---
+++ day2/main_test.go
@@ -0,0 +1,29 @@
+package day2
+
+import "testing"
+
+func TestScoreRound(t *testing.T) {
+	var data = []struct {
+		myShape       Shape
+		opponentShape Shape
+		expectedScore int
+	}{
+		{Rock, Rock, 3},
+		{Rock, Scissors, 6},
+		{Paper, Scissors, 0},
+	}
+
+	for _, tt := range data {
+		score := ScoreRound(tt.opponentShape, tt.myShape)
+		if score != tt.expectedScore {
+			t.Errorf("Unexpected score: %d,", tt)
+		}
+	}
+}
+
+func TestDay2(t *testing.T) {
+	got := Day2()
+	if got != 12586 {
+		t.Errorf("Unexpected result: %d", got)
+	}
+}
