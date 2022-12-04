2a4c3baa279c11ab1a640f0a1a028eb9ddf1a9d9
Ivaylo Bratoev <ivo@st6.io>
2022-12-04 19:27:45 +0200

    Day 4

---
+++ day4/main.go
@@ -0,0 +1,65 @@
+package day4
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+type Range struct {
+	start int
+	end   int
+}
+
+func (r *Range) Contains(other *Range) bool {
+	return r.start <= other.start && r.end >= other.end
+}
+
+func (r *Range) Overlap(other *Range) bool {
+	return r.Contains(&Range{other.start, other.start}) ||
+		r.Contains(&Range{other.end, other.end})
+}
+
+func parseRange(rangeString string) Range {
+	var parsed = strings.Split(rangeString, "-")
+	if start, errStart := strconv.Atoi(parsed[0]); errStart == nil {
+		if end, errEnd := strconv.Atoi(parsed[1]); errEnd == nil {
+			return Range{start, end}
+		}
+	}
+
+	panic(fmt.Sprintf("Error parsing start: %v", rangeString))
+}
+
+func parseRanges(line string) (Range, Range) {
+	var ranges = strings.Split(line, ",")
+	return parseRange(ranges[0]), parseRange(ranges[1])
+}
+
+func Day4Part1() int {
+	var sum = 0
+	for _, line := range strings.Split(strings.TrimRight(input, "\n"), "\n") {
+		first, second := parseRanges(line)
+		if first.Contains(&second) || second.Contains(&first) {
+			sum++
+		}
+	}
+
+	return sum
+}
+
+func Day4Part2() int {
+	var sum = 0
+	for _, line := range strings.Split(strings.TrimRight(input, "\n"), "\n") {
+		first, second := parseRanges(line)
+		if first.Overlap(&second) {
+			sum++
+		}
+	}
+
+	return sum
+}
