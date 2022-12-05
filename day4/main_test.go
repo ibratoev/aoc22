c14135bfe161818cdd48b46c3ba66465c687da96
Ivaylo Bratoev <ivo@st6.io>
2022-12-05 16:28:35 +0200

    Day 4 (#4)
    * Day 4
    
    * Fix Overlaps
    
    * Optimize Overlaps
    
    * minor wording fix

---
+++ day4/main_test.go
@@ -0,0 +1,56 @@
+package day4
+
+import "testing"
+
+func TestParseRange(t *testing.T) {
+	var data = []struct {
+		rangeStr string
+		expected Range
+	}{
+		{"2-4", Range{2, 4}},
+		{"2-2", Range{2, 2}},
+		{"0-6", Range{0, 6}},
+	}
+
+	for _, tt := range data {
+		parsed := parseRange(tt.rangeStr)
+		if parsed != tt.expected {
+			t.Errorf("Unexpected range: %+v,", tt)
+		}
+	}
+}
+
+func TestRangeOverlap(t *testing.T) {
+	var data = []struct {
+		ranges   [2]Range
+		overlaps bool
+	}{
+		{[2]Range{{5, 7}, {7, 9}}, true},
+		{[2]Range{{2, 8}, {3, 7}}, true},
+		{[2]Range{{2, 8}, {9, 10}}, false},
+		{[2]Range{{4, 6}, {6, 6}}, true},
+		{[2]Range{{2, 6}, {4, 8}}, true},
+		{[2]Range{{3, 7}, {2, 8}}, true},
+	}
+
+	for _, tt := range data {
+		overlaps := tt.ranges[0].Overlaps(&tt.ranges[1])
+		if overlaps != tt.overlaps {
+			t.Errorf("Unexpected: %+v,", tt)
+		}
+	}
+}
+
+func TestDay4Part1(t *testing.T) {
+	got := Day4Part1()
+	if got != 651 {
+		t.Errorf("Unexpected result: %d", got)
+	}
+}
+
+func TestDay4Part2(t *testing.T) {
+	got := Day4Part2()
+	if got != 956 {
+		t.Errorf("Unexpected result: %d", got)
+	}
+}
